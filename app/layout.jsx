e2731d040a4f503a2be3b1917997d28803ae8c42
Xintong Shi <xintongshi@Little-Miss-Powerhouse.local>
2024-07-18 15:33:12 -0700

    changed metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,8 @@ import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
 
 export const metadata = {
-  title: 'Next.js + Three.js',
-  description: 'A minimal starter for Nextjs + React-three-fiber and Threejs.',
+  title: 'AnimatedReality',
+  description: 'A creative studio, working with XR and Protopias',
 }
 
 export default function RootLayout({ children }) {
